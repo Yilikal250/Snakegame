13788565ea8a0ee331a7beb9d77568348fb0b5ef
Yilikal250 <teshomeyilikal250@gmail.com>
2022-11-29 10:55:52 +0300

    added a shift to make the snake disapear

--- snake.rb
+++ snake.rb
@@ -1,9 +1,11 @@
 require 'ruby2d'
 GRID_SIZE = 20
-
+set background: 'navy'
+set fps_cap: 1
 class Snake
   def initialize
     @position = [[2, 0], [2, 1], [2, 2], [2, 3]]
+    @direction='down'   
   end
 
   def draw
@@ -11,8 +13,21 @@ class Snake
       Square.new(x: position[0] * GRID_SIZE, y: position[1] * GRID_SIZE, size: GRID_SIZE - 1, color: 'white')
     end
   end
+
+  def move
+  case @direction
+  when 'down'
+    @position.shift
+  end    
+  end  
 end
 
 snake = Snake.new
 snake.draw
+
+update do
+clear
+snake.move
+snake.draw
+end    
 show

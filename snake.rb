a1577270eec924b4482dfdf4a72abfa5cf0c7e5a
Yilikal250 <teshomeyilikal250@gmail.com>
2022-11-29 13:10:31 +0300

    added event key and directions to the movement

--- snake.rb
+++ snake.rb
@@ -3,10 +3,11 @@ GRID_SIZE = 20
 set background: 'navy'
 set fps_cap: 20
 class Snake
-    attr_writer :direction
+  attr_writer :direction
+
   def initialize
     @position = [[2, 0], [2, 1], [2, 2], [2, 3]]
-    @direction='down'   
+    @direction = 'down'
   end
 
   def draw
@@ -16,47 +17,46 @@ class Snake
   end
 
   def move
-  @position.shift  
-  case @direction
-  when 'down'
-    @position.push([head[0],head[1]+1])
-  when'up'  
-    @position.push([head[0],head[1]-1])
-  when'left'  
-    @position.push([head[0]-1,head[1]])  
-  when'right'  
-    @position.push([head[0]+1,head[1]])    
-  end    
-  end 
+    @position.shift
+    case @direction
+    when 'down'
+      @position.push([head[0], head[1] + 1])
+    when 'up'
+      @position.push([head[0], head[1] - 1])
+    when 'left'
+      @position.push([head[0] - 1, head[1]])
+    when 'right'
+      @position.push([head[0] + 1, head[1]])
+    end
+  end
+
   def can_change_direction_to?(new_direction)
-   case @direction
-   when 'up' then new_direction != 'down'
-   when 'down' then new_direction !='up'
-   when 'left' then new_direction != 'right'
-   when 'right' then new_direction !='left'
-   end     
-  end  
-   private
+    case @direction
+    when 'up' then new_direction != 'down'
+    when 'down' then new_direction != 'up'
+    when 'left' then new_direction != 'right'
+    when 'right' then new_direction != 'left'
+    end
+  end
+
+  private
+
   def head
     @position.last
-  end  
+  end
 end
 
 snake = Snake.new
 snake.draw
 
 update do
-clear
-snake.move
-snake.draw
-end    
+  clear
+  snake.move
+  snake.draw
+end
 
 on :key_down do |event|
-    if ['up','down','left','right'].include?(event.key)
-    if snake.can_change_direction_to?(event.key)
-    snake.direction=event.key
-    end
-    end 
-end   
+  snake.direction = event.key if %w[up down left right].include?(event.key) && snake.can_change_direction_to?(event.key)
+end
 
 show
